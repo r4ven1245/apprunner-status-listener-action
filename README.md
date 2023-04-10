777594b51b81179a49e8862ab5916b9b529e786d
Mykhailo Kushnir <mkv.ysk@gmail.com>
2023-04-10 15:55:19 +0300

    1.0.0 initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# AppRunner Status Listener Action
+
+This action awaits for a specific status of the AppRunner service or times out, giving you control of the deployment
+
+## Inputs
+
+### `aws-access-key-id`
+
+**Required** AWS Authentication Data.
+
+### `aws-secret-access-key`
+
+**Required** AWS Authentication Data.
+
+### `aws-region`
+
+**Required** AWS Authentication Data.
+
+### `app-arn`
+
+**Required** AppRunner service ARN.
+
+### `desired-status`
+
+**Required** Desired AppRunner service status, validated against official documentation at https://docs.aws.amazon.com/apprunner/latest/api/API_Service.html.
+
+### `timeout`
+
+Timeout in ms. `0` means no timeout. Default `30000`.
+
+## Outputs
+
+### `timed-out`
+
+`true` if the desired status wasn't received before the timeout occurred
+
+## Example usage
+
+```yaml
+uses: r4ven1245/apprunner-status-listener-action@v1.0.0
+with:
+    aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+    aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+    aws-region: ${{ env.AWS_REGION }}
+    app-arn: 'arn:aws:apprunner:eu-west-1:XXXXXXXXXXXX:service/XXXXX/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    desired-status: 'RUNNING'
+    timeout: 0
+```
+
+```yaml
+uses: r4ven1245/apprunner-status-listener-action@v1.0.0
+with:
+    aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+    aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+    aws-region: ${{ env.AWS_REGION }}
+    app-arn: 'arn:aws:apprunner:eu-west-1:XXXXXXXXXXXX:service/XXXXX/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    desired-status: 'OPERATION_IN_PROGRESS'
+    timeout: 15000
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+import * as core from '@actions/core';
+import { AppRunnerClient, DescribeServiceCommand } from '@aws-sdk/client-apprunner';
+
+try
+{
+  //Setup
+  const arn = core.getInput(`app-arn`);
+  const desiredStatus = core.getInput(`desired-status`).toUpperCase();
+
+  const allowedStatuses = [
+    `CREATE_FAILED`,
+    `RUNNING`,
+    `DELETED`,
+    `DELETE_FAILED`,
+    `PAUSED`,
+    `OPERATION_IN_PROGRESS`,
+  ];
+
+  if(!allowedStatuses.includes(desiredStatus))
+  {
+    throw new Error(`ERROR - 'desired-status' input must be one of the following: '${allowedStatuses.join(`' , '`)}'`);
+  }
+
+  const minTimeout = 0;
+
+  const sleepTime = 1000; //ms
+
+  const timeout = Math.min(parseInt(core.getInput(`timeout`)), minTimeout);
+
+  const client = new AppRunnerClient({
+    region: core.getInput(`aws-region`),
+    credentials: {
+      accessKeyId: core.getInput(`aws-access-key-id`),
+      secretAccessKey: core.getInput(`aws-secret-access-key`)
+    }
+  });
+
+  const command = new DescribeServiceCommand({ ServiceArn: arn });
+
+  const startTime = Date.now();
+
+  //Run
+
+  let success = false;
+
+  while(timeout === 0 || startTime + timeout < Date.now())
+  {
+    const response = await client.send(command);
+    const responseStatus = response.Service.Status;
+
+    if(responseStatus === desiredStatus)
+    {
+      console.log(`SUCCESS - Status is desired '${desiredStatus}'`);
+      success = true;
+      break;
+    }
+    else
+    {
+      console.log(`CONTINUE - Status is '${desiredStatus}'`);
+    }
+
+    await new Promise(resolve => setTimeout(resolve, sleepTime));
+  }
+
+  //Outputs
+
+  core.setOutput(`timed-out`, !success);
+}
+catch(err)
+{
+  core.setFailed(err.message);
+}
